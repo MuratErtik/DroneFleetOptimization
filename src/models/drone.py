e53987e29d16ab0d4001a27f7bc6fccb62b7aa41
Murat Ertik <dmuratertik1@gmail.com>
2025-05-22 02:05:14 -0700

    drone model has been completed.

---
+++ src/models/drone.py
@@ -0,0 +1,10 @@
+class Drone:
+    def __init__(self, id, max_weight, battery, speed, start_pos):
+        self.id = id                       # (int)
+        self.max_weight = max_weight       # max kg for shipping item for drone able to take it (float)
+        self.battery = battery             # mAh (int)
+        self.speed = speed                 # m\s (int)
+        self.start_pos = start_pos         # (x,y) tuple
+        self.battery_level = battery       # current battery level (int)
+        self.charging_time = 0             # charging time (minute,int)
+        self.current_pos = start_pos       # current position (at the beggining start_pos)
